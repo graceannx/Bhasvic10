5b45efc736ee447a6807046aaa632f382e6ade66
Grace Richardson <Graceannx@icloud.com>
2016-11-18 01:03:07 +0000

    added some things

--- iOS/secondViewController_.cs
+++ iOS/secondViewController_.cs
@@ -5,26 +5,16 @@ using UIKit;
 
 namespace Bhasvic10th.iOS
 {
-    public partial class secondViewController: UIViewController
+    public partial class SecondViewController: UIViewController
     {
-        public secondViewController (IntPtr handle) : base (handle)
+		public SecondViewController (IntPtr handle) : base (handle)
         {
         }
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
-			var button = new UIButton
-			{
-				Frame = new CGRect(0, 50, View.Bounds.Width, 30),
-				BackgroundColor = UIColor.Purple
-			};
-			button.SetTitle("hello", UIControlState.Normal);
-			View.Add(button);
-			button.TouchUpInside += (sender, e) =>
-			{
 
-			}
-			;
+
 
 		}
     }
