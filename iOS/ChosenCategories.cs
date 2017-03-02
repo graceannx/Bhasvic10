db299b68d7e4aea320e885a10eef06cdf2c867d4
GraceRichardson <graceannx@icloud.com>
2017-03-02 19:52:17 +0000

    added more

---
+++ iOS/ChosenCategories.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using SQLite;
+namespace Bhasvic10th.iOS
+{
+	public class ChosenCategories
+	{
+		[PrimaryKey]
+		string Category { get; set; }
+	}
+}
